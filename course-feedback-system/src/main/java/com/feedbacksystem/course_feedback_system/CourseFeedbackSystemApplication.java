8f147375eff2c33b9d8ea4683093f2bcb8173c00
khaled-KMH <khaledek767403@gmail.com>
2025-05-01 00:30:46 +0300

    Initial commit

---
+++ course-feedback-system/src/main/java/com/feedbacksystem/course_feedback_system/CourseFeedbackSystemApplication.java
@@ -0,0 +1,13 @@
+package com.feedbacksystem.course_feedback_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseFeedbackSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseFeedbackSystemApplication.class, args);
+	}
+
+}
---
+++ course-feedback-system/src/test/java/com/feedbacksystem/course_feedback_system/CourseFeedbackSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feedbacksystem.course_feedback_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseFeedbackSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
