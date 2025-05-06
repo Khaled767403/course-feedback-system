d7d7e52185d60f21cae6b0f2dfdec7e2e8b02468
khaled-KMH <khaledek767403@gmail.com>
2025-05-06 18:16:34 +0300

    Fresh start - added frontend application

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/api/users') // ده الـ Gateway
+      .then(response => {
+        setUsers(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching users:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Users List:</h1>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
+
